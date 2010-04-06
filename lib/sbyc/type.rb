89959d7f9d793c8a6f7420ccd3ba6d3f14862658
Bernard Lambeau <blambeau@gmail.com>
2010-04-06 10:25:25 +0200

    Updates the README with an example.

--- lib/sbyc/type.rb
+++ lib/sbyc/type.rb
@@ -1,11 +1,12 @@
 module SByC
   module Type
 
+    # Raises a TypeError.
     def selector_invocation_error(clazz, value)
       raise TypeError, "Invalid selector invocation #{clazz}[#{value}]", caller
     end
       
-    # Checks if a value belongs to this type
+    # Checks if a value belongs to this type, returning true or false, accordingly.
     def belongs_to?(value)
       return false if (superclass.respond_to?(:belongs_to?) and not(superclass.belongs_to?(value)))
       if self.respond_to?(:check_type_constraints)
@@ -16,8 +17,14 @@ module SByC
     end
     alias :=== :belongs_to?
     
+    # Creates a subtype of this type based on a constraint given by the block.
     def sbyc(&constraint)
       raise ::SByC::TypeSystemError, "#{self} should impement sbyc"
+    end
+    
+    # Alias for sbyc
+    def such_that(&constraint)
+      sbyc(&constraint)
     end
 
   end # module Type
