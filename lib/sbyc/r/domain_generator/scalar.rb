93894dcf8465c66e587ac7dd54b6cf55784c715e
Bernard Lambeau <blambeau@gmail.com>
2010-08-13 18:51:41 +0200

    Starts implementation of scalar domains.

---
+++ lib/sbyc/r/domain_generator/scalar.rb
@@ -0,0 +1,29 @@
+module SByC
+  module R
+    class DomainGenerator
+      class Scalar < DomainGenerator
+        
+        def domain_name_of(domain)
+          domain.domain_name
+        end
+        
+        def selector_signature(domain)
+          SByC::R::Operator::Signature::paired(SByC::R::Symbol, SByC::R::Alpha)
+        end
+        
+        def generate(name, heading)
+          domain = factor_domain_class([Scalar::ScalarDomain])
+          domain.heading = R::Heading.new(heading)
+          domain.domain_name = name
+          domain
+        end
+        
+        def refine(domain, *args)
+          raise NotImplementedError, "Unable to refine array domains"
+        end
+        
+      end # class Array
+    end # class DomainGenerator
+  end # module R
+end # module SByC
+require 'sbyc/r/domain_generator/scalar/scalar_domain'
---
+++ lib/sbyc/r/domain_generator/scalar/scalar_domain.rb
@@ -0,0 +1,65 @@
+class SByC::R::DomainGenerator::Builtin
+  module ScalarDomain
+    module ClassMethods
+      
+      attr_accessor :domain_name
+      attr_accessor :heading
+    
+      def exemplars
+        [ self.new(:name => "blambeau", :age => 30) ]
+      end
+    
+      def is_value?(value)
+        value.class == self
+      end
+
+      def parse_literal(str)
+        if str =~ /^\(#{domain_name} /
+          begin
+            x = R::evaluate({}, str)
+            if is_value?(x)
+              x
+            else
+              __not_a_literal__!(self, str)
+            end
+          rescue CodeTree::ParseError
+            __not_a_literal__!(self, str)
+          end
+        else
+          __not_a_literal__!(self, str)
+        end
+      end
+
+      def to_literal(value)
+        value.to_s
+      end
+      
+    end
+    module InstanceMethods
+      
+      # Used hash
+      attr_reader :hash
+      
+      # Creates a scalar domain instance
+      def initialize(hash)
+        @hash = hash
+      end
+      
+      def to_s
+        buffer, first = "(#{self.class.domain_name} ", true
+        hash.each_pair{|k,v|
+          buffer << ", " unless first 
+          buffer << k.inspect << " " << v.sbyc_domain.to_literal(v)
+          first = false
+        }
+        buffer << ")"
+        buffer
+      end
+      
+      def ==(other)
+        other.kind_of?(self.class) and other.hash == self.hash
+      end
+      
+    end
+  end # module ScalarDomain
+end # class SByC::R::DomainGenerator::Builtin
