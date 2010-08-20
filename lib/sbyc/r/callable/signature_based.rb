31ba2d6c8786ba668b0a90a21f58231e202f2975
Bernard Lambeau <blambeau@gmail.com>
2010-08-20 19:04:16 +0200

    Removes argument ensure to SignatureBased methods.

--- lib/sbyc/r/callable/signature_based.rb
+++ lib/sbyc/r/callable/signature_based.rb
@@ -8,15 +8,8 @@ module SByC
           @arity ||= call_signature.size
         end
       
-        def ensure_call_args(runner, args, binding)
-          args = runner.ensure_args(args, call_signature(runner, args, binding), binding){
-            call_error(runner, args, binding)
-          }
-          yield(args)
-        end
-      
         def sbyc_call(runner, args, binding)
-          ensure_call_args(runner, args, binding){|coerced| coerce(runner, coerced, binding)}
+          coerce(runner, args, binding)
         end
 
       end # module AstBased
